3ec5c0d729cadd10b5d156895e7e7e014cdec65d
Dimitris Tsapetis <dimtsap@hotmail.com>
2019-10-09 16:30:43 +0300

    Update StructuralSystemEvaluator.cs

--- src/MGroup.Stohastic.Structural/StructuralSystemEvaluator.cs
+++ src/MGroup.Stohastic.Structural/StructuralSystemEvaluator.cs
@@ -11,10 +11,7 @@ namespace MGroup.Stochastic.Structural
     {
         public double YoungModulus { get; }
         public IStochasticDomainMapper DomainMapper;
-        //public RandomVariable StochasticRealization { get; }
         public KarhunenLoeveCoefficientsProvider StochasticRealization { get; }
-        //public SpectralRepresentation1DRandomFieldGenerator StochasticRealization { get; }
-        //public ModelBuilder ModelBuilder { get; }
         public GiannisModelBuilder ModelBuilder { get; }
         private Model currentModel;
         int karLoeveTerms = 4;
@@ -27,14 +24,6 @@ namespace MGroup.Stochastic.Structural
         int PCorder = 1;
         bool midpointMethod = true;
 
-        //public StructuralSystemEvaluator(double youngModulus, IStochasticDomainMapper domainMapper)
-        //{
-        //    YoungModulus = youngModulus;
-        //    DomainMapper = domainMapper;
-        //    ModelBuilder = new ModelBuilder(); 
-        //    StochasticRealization = new RandomVariable(youngModulus, domainMapper);
-        //}
-
         /// <summary>Initializes a new instance of the <see cref="StructuralSystemEvaluator"/> class.</summary>
         /// <param name="youngModulus">The young modulus.</param>
         /// <param name="domainMapper">The domain mapper.</param>
@@ -45,7 +34,6 @@ namespace MGroup.Stochastic.Structural
             ModelBuilder = new GiannisModelBuilder();
             StochasticRealization = new KarhunenLoeveCoefficientsProvider(partition, youngModulus, midpointMethod,
                 isGaussian, karLoeveTerms, domainBounds, sigmaSquare, correlationLength);
-            //StochasticRealization = new SpectralRepresentation1DRandomFieldGenerator(10, 0.1, youngModulus, .05, 0.1, 256);
         }
 
         /// <summary>Realizes the specified iteration.</summary>
@@ -62,9 +50,6 @@ namespace MGroup.Stochastic.Structural
         /// <returns></returns>
         public double[] Evaluate(int iteration)
         {
-            //TODO: Perhaps the analyzer, solver, etc should be reused throughout the MonteCarlo, to avoid recalculating 
-            //      connectivity and indexing structures.
-
             var solver = new SkylineSolver.Builder().BuildSolver(currentModel);
             var provider = new ProblemStructural(currentModel, solver);
             var childAnalyzer = new LinearAnalyzer(currentModel, solver, provider);
@@ -72,7 +57,6 @@ namespace MGroup.Stochastic.Structural
 
             parentAnalyzer.Initialize();
             parentAnalyzer.Solve();
-            //return new[] { linearSystems[0].RHS[0] };
             return new[] { solver.LinearSystems[0].Solution[56], solver.LinearSystems[0].Solution[58] };
         }
 
