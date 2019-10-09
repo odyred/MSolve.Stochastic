8ce63506e84b3619bcd481281e400f804d28dcde
Odysseas Kokkinos <odyred@gmail.com>
2019-10-09 14:26:35 +0300

    added comparison check in CantileverWithUncertainMaterialProperties

--- src/MGroup.Stohastic/MonteCarlo.cs
+++ src/MGroup.Stohastic/MonteCarlo.cs
@@ -1,15 +1,15 @@
-﻿using System;
 using MGroup.Stochastic.Interfaces;
+using System;
 
 namespace MGroup.Stochastic
 {
     public class MonteCarlo
     {
         public int NoOfIterations { get; }
-        public double[] MeanValueResponse;
-        public double[] StandardDeviationResponse;
         public ISystemRealizer SystemRealizer { get; }
         public ISystemResponseEvaluator SystemResponseEvaluator { get; }
+        public double[] MonteCarloMeanValue;
+        public double[] MonteCarloStandardDeviation;
 
         /// <summary>Initializes a new instance of the <see cref="MonteCarlo"/> class.</summary>
         /// <param name="noOfIterations">The no of iterations.</param>
@@ -26,13 +26,14 @@ namespace MGroup.Stochastic
         public void Evaluate()
         {
             SystemRealizer.Realize(0);
-            double[] systemResponse = SystemResponseEvaluator.Evaluate(0);
-            int systemResponseDimension = systemResponse.Length;
-            MeanValueResponse = new double[systemResponseDimension];
-            StandardDeviationResponse = new double[systemResponseDimension];
+            int systemResponseDimension = SystemResponseEvaluator.Evaluate(0).Length;
+            var systemResponse = new double[systemResponseDimension];
+            MonteCarloMeanValue = new double[systemResponseDimension];
+            MonteCarloStandardDeviation = new double[systemResponseDimension];
+            systemResponse = SystemResponseEvaluator.Evaluate(0);
             for (int i = 0; i < systemResponse.Length; i++)
             {
-                MeanValueResponse[i] += systemResponse[i];
+                MonteCarloMeanValue[i] += systemResponse[i];
             }
 
             for (int i = 1; i < NoOfIterations; i++)
@@ -41,15 +42,15 @@ namespace MGroup.Stochastic
                 systemResponse = SystemResponseEvaluator.Evaluate(i);
                 for (int j = 0; j < systemResponse.Length; j++)
                 {
-                    MeanValueResponse[j] += systemResponse[j];
+                    MonteCarloMeanValue[j] += systemResponse[j];
                 }
             }
 
             for (int i = 0; i < systemResponse.Length; i++)
             {
-                MeanValueResponse[i] = MeanValueResponse[i] / NoOfIterations;
-                StandardDeviationResponse[i] = (systemResponse[i] - MeanValueResponse[i]) * (systemResponse[i] - MeanValueResponse[i]);
-                StandardDeviationResponse[i] = Math.Sqrt(StandardDeviationResponse[i] / NoOfIterations);
+                MonteCarloMeanValue[i] = MonteCarloMeanValue[i] / NoOfIterations;
+                MonteCarloStandardDeviation[i] = (systemResponse[i] - MonteCarloMeanValue[i]) * (systemResponse[i] - MonteCarloMeanValue[i]);
+                MonteCarloStandardDeviation[i] = Math.Sqrt(MonteCarloStandardDeviation[i] / NoOfIterations);
             }
         }
     }
--- tests/CantileverWithUncertainMaterialProperties.cs
+++ tests/CantileverWithUncertainMaterialProperties.cs
@@ -14,6 +14,11 @@ namespace MGroup.Stochastic.Structural.Example
             var evaluator = new StructuralStochasticEvaluator(youngModulus, domainMapper);
             var m = new MonteCarlo(iterations, realizer, evaluator);
             m.Evaluate();
+            Assert.Equal(1.5999674517697445E-08, MonteCarloMeanValue[0], 10);
+            Assert.Equal(-2.399309224401548E-08, MonteCarloMeanValue[1], 10);
+            Assert.Equal(4.3180461976960273E-12, MonteCarloStandardDeviation[0], 10);
+            Assert.Equal(2.6374139668652252E-12, MonteCarloStandardDeviation[1], 10);
+
         }
     }
 }
